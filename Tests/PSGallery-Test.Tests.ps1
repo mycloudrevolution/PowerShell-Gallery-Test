63860088c40c8d436c014aadac9ec95af5daf026
mycloudrevolution <markus.kraus@gmail.com>
2016-06-01 15:43:36 +0200

    Base Pester

---
+++ Tests/PSGallery-Test.Tests.ps1
@@ -0,0 +1,5 @@
+﻿Describe 'Module Test' {
+    It 'Tests the Output of my Module' {
+        Install-Module -Name PSGallery-Test -Force; Install-Module -Name PSGallery-Test; Get-Awsome | Should Be 'Get Awsome, Learn PowerShell! (http://mycloudrevolution.com/)' 
+    }
+}
